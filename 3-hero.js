a6d3e4be08ee2d61714974b911ac7f88116f37cd
Restu <restuwibowo16@gmail.com>
2024-01-30 14:16:19 +0700

    challenge 3/366

---
+++ 3-hero.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c
+
+// function hero(bullets, dragons) {
+//   return bullets / 2 >= dragons
+// }
+
+const hero = (bullets, dragons) => bullets / 2 >= dragons
+console.log(hero(10, 5));
